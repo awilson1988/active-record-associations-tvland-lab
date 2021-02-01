7f44eec6c8ff712347e5935b1595342023b6eaed
amanda <amanda.blaze1988@gmail.com>
2021-01-31 17:16:37 -0800

    commit tree bcf89b702537831d3fd24867ff01747fb9c9b85c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+ 	has_many :actors, through: :characters
+
+  def full_name
+    result = []
+    result << self.first_name
+    result << self.last_name
+    result.join(' ')
+  end
+
+  def list_roles
+    characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+
+
+
+end 
+
   
-end
